62ed51f89061fd0f7f1f3996971ce82af40c4db1
Adem Aytaç <adem@aytacworld.com>
2018-03-27 23:12:10 +0200

    incremental

--- install/install-general.sh
+++ install/install-general.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install all packages
-sudo pacman -S wget unzip tar git pulseaudio pulseaudio-alsa  xorg xorg-xinit plasma-desktop konsole dolphin kate iceweasel iceweasel-l10n-nl icedove icedove-enigmail icedove-l10n-nl bash-completion sddm aspell-nl enchant gst-libav gst-plugins-good hunspell-nl icedtea-web jre8-openjdk languagetool libmythes mythes-nl kscreen ttf-dejavu ttf-droid ttf-gentium ttf-liberation breeze-gtk breeze-kde4 kde-gtk-config kdeplasma-addons  plasma-pa ark kinfocenter kwalletmanager gwenview kipi-plugins gimp spectacle libreoffice-fresh okular amarok vlc speedcrunch redshift plasma5-applets-redshift-control digikam kcolorchooser kruler kompare kfind ktorrent ufw networkmanager plasma-nm bluez bluez-utils kdeconnect xsane libreoffice-fresh libreoffice-fresh-nl
+sudo pacman -S wget unzip tar git pulseaudio pulseaudio-alsa  xorg xorg-xinit plasma-desktop konsole dolphin kate iceweasel iceweasel-l10n-nl icedove icedove-enigmail icedove-l10n-nl bash-completion sddm aspell-nl enchant gst-libav gst-plugins-good hunspell-nl icedtea-web jre8-openjdk languagetool libmythes mythes-nl kscreen ttf-dejavu ttf-droid ttf-gentium ttf-liberation breeze-gtk breeze-kde4 kde-gtk-config kdeplasma-addons  plasma-pa ark kinfocenter kwalletmanager gwenview kipi-plugins gimp spectacle libreoffice-fresh okular amarok vlc speedcrunch redshift plasma5-applets-redshift-control digikam kcolorchooser kruler kompare kfind ktorrent ufw networkmanager plasma-nm bluez bluez-utils kdeconnect xsane libreoffice-fresh libreoffice-fresh-nl openssh
 
 # Create startup file
 echo “exec startkde” > ~/.xinitrc
